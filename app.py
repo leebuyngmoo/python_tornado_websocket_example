316c3e786fe6c453441a995fe2d80afacac1dea4
leebuyngmoo <am003507@gmail.com>
2018-03-27 13:38:49 +0900

    init project

---
+++ app.py
@@ -0,0 +1,56 @@
+from tornado import websocket, web, ioloop
+import json
+
+cl = [] # web socket client list
+
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class SocketHandler(websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        print "check_origin"
+        return True
+
+    def open(self):
+        print "open"
+        print cl
+        if self not in cl:
+            cl.append(self)
+
+    def on_close(self):
+        print "opn_close"
+        print cl
+        if self in cl:
+            cl.remove(self)
+
+class ApiHandler(web.RequestHandler):
+
+    @web.asynchronous
+    def get(self, *args):
+        print "get"
+        print "get cl == ",cl
+        self.finish()
+        id = self.get_argument("id")
+        value = self.get_argument("value")
+        data = {"id": id, "value" : value}
+        data = json.dumps(data)
+        for c in cl:
+            c.write_message(data)
+
+    @web.asynchronous
+    def post(self):
+        print "post"
+        pass
+
+app = web.Application([
+    (r'/', IndexHandler),
+    (r'/ws', SocketHandler),
+    (r'/api', ApiHandler),
+    (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
+    (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
+])
+
+if __name__ == '__main__':
+    app.listen(9999)
+    ioloop.IOLoop.instance().start()
